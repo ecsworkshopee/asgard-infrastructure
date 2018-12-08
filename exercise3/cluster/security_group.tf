13af96a13f09a2cd64c8dda9801f4290761467c9
Rajan Patil <rpatil@equalexperts.com>
2018-12-08 16:13:44 +0530

    Added security group to allow access only from alb

--- exercise3/cluster/security_group.tf
+++ exercise3/cluster/security_group.tf
@@ -15,6 +15,11 @@ resource "aws_security_group" "ecs-instance-security" {
   }
 }
 
+data "aws_alb" "alb" {
+  name = "${local.unique_id}-asgard-alb"
+}
+
+
 resource "aws_security_group_rule" "allow_all_traffic_ephemeral_port_range" {
   from_port = 31000
   protocol = "tcp"
@@ -22,7 +27,8 @@ resource "aws_security_group_rule" "allow_all_traffic_ephemeral_port_range" {
   to_port = 61000
   type = "ingress"
 
-  cidr_blocks = ["0.0.0.0/0"]
+  //cidr_blocks = ["0.0.0.0/0"]
+  source_security_group_id = "${element(data.aws_alb.alb.security_groups, 0)}"
 }
 
 resource "aws_security_group_rule" "allow_outgoing_traffic" {
